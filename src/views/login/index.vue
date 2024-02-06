9de216ce110f853f96d54b21e71ac9b48804880d
xixiibn5100 <1584914306@qq.com>
2024-02-06 20:21:59 +0800

    feat(login):添加了登陆页面的身份转换,加入了动画包

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -12,21 +12,30 @@
       <input type="text" class="username" placeholder="username" v-model='username' />
       <input type="password" class="password" placeholder="password" v-model='password'/>
       <button class="loginButton" v-on:click='login'>Go</button>
+      <div class="switchBox">
+      <div class="switch1" @click="setIdentity('teacher')">我是{{ type_ === 'teacher' ? '学生' : '老师' }}</div>
+      <div class="switch2" @click="setIdentity('administrator')">我是{{ type_ === 'administrator' ? '学生' : '管理员' }}</div>
+      </div>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
 import "./index.scss";
-import {ref} from "vue";
-import { startLoading,closeLoading } from "@/tool/index"
+import { ref } from "vue";
+import { startLoading, closeLoading } from "@/tool/index";
+import 'animate.css';
 
 const username = ref("");
 const password = ref("");
+const type_ = ref("");
 
 const login = async () => {
   startLoading();
   closeLoading();
 };
 
+const setIdentity = (identity: string) => {
+  type_.value = identity === type_.value ? '' : identity;
+};
 </script>
