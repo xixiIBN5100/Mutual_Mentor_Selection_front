0afe981bdd254a6d6c8ac2ac58ee4cbe57efee2f
yzyshimisi <3198685096@qq.com>
2024-02-07 10:50:03 +0800

    3

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,16 @@ import router from './router'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+<<<<<<< HEAD
 
 const app = createApp(App)
 for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
     app.component(key, component)
+=======
+const app = createApp(App)
+for (const [key, component] of Object.entries(ElementPlusIconsVue)){
+  app.component(key, component)
+>>>>>>> 0cc4715b609a7328dec4bc89f91e01697171871e
 };
 app.use(createPinia())
 app.use(router)
