d7fde13d3d6df290327c5a87b1a0e393b4065c8f
xixiibn5100 <1584914306@qq.com>
2024-02-07 18:29:58 +0800

    feat(approval):新增了教师审批的页面配置与骨架

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import { Login, Home,} from '@/views';
+import { Login, Home, Approval} from '@/views';
 
 const routes = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -13,6 +13,11 @@ const routes = createRouter({
       path: '/login',
       name: 'login',
       component: Login
+    },
+    {
+      path: '/approval',
+      name: 'approval',
+      component: Approval
     },
     {
       path:'/',
