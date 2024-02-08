bbca70429feaf3b3ed2d1b629c99b62773ed6e79
RosyrRais <syr050301@qq.com>
2024-02-08 21:05:17 +0800

    feat(passwordChange): 增加了修改密码页面

--- src/stores/service/userStore.ts
+++ src/stores/service/userStore.ts
@@ -21,29 +21,29 @@ export interface userSessionType {
 }
 
 const useUserStore = defineStore("user",() => {
-  const userIdentity = ref<string>();
-  const userSession = reactive<userSessionType>({});
+  // const userIdentity = ref<string>();
+  // const userSession = reactive<userSessionType>({});
 
   /* --- 测试用数据 --- */
-  // const userIdentity = ref<string>("学生");
+  const userIdentity = ref<string>("学生");
   // userSession.student_id = 114514;
-  // const userSession = reactive<userSessionType>({
-  //   name:"Rosyr",
+  const userSession = reactive<userSessionType>({
+    name:"Rosyr",
 
-  //   student_id: 114514,
-  //   class: "1班",
-  //   phone: 13711451419,
-  //   political_status: "群众",
-  //   email: "syr050301@qq.com",
-  //   address:"xx区x-x-x0x",
-  //   plan: "",
-  //   experience: "",
-  //   honor: "1",
-  //   interest: "出国",
+    student_id: 114514,
+    class: "1班",
+    phone: 13711451419,
+    political_status: "群众",
+    email: "syr050301@qq.com",
+    address:"xx区x-x-x0x",
+    plan: "",
+    experience: "",
+    honor: "1",
+    interest: "出国",
 
-  //   teacher_id: -1,
-  //   admin_id: -1,
-  // });
+    teacher_id: -1,
+    admin_id: -1,
+  });
   /* --- 测试用数据 --- */
 
   const setUserIdentity = (identity: string) => {
