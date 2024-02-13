098870989d5ec1c1b5942e1be863e241c57164b4
yzyshimisi <3198685096@qq.com>
2024-02-13 10:46:01 +0800

    123

--- src/apis/Server/getTeacherInfo.ts
+++ src/apis/Server/getTeacherInfo.ts
@@ -1,10 +1,10 @@
 import request from '@/apis/request'
 
 export default class getTeacherInfo {
-  static async getInfo(params:[
-    page_num:number,
-    page_size:number,
-  ]){
+  static async getInfo(params: {
+    page_num: number,
+    page_size: number,
+  }){
     return request({
       headers:{
         "Content-Type":"application/json",
