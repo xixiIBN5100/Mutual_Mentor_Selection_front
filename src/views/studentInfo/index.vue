8a1185ef38d492e71d87b6d3fcaf705144e09f19
xixiibn5100 <1584914306@qq.com>
2024-02-23 16:55:07 +0800

    feat(studentinfo):新增老师查看学生信息页面

--- src/views/studentInfo/index.vue
+++ src/views/studentInfo/index.vue
@@ -4,24 +4,27 @@
       <card :class="styles['title-bar']" title="个人信息" :bold-title="true" :is-fading-out=isFadingOut>
         <el-icon :class="styles['back-button']" :size="30" @click="() => jumpPage('/approval')"><Back /></el-icon>
       </card>
-      <card :class="styles['info-editer']" title="学生个人信息" :is-fading-out=isFadingOut>
-        <el-icon :class="styles['background-icon']" :size="200" color="#a0cddf"><EditPen /></el-icon>
-        <div :class="styles['info-editer-contain']">
-          <div :class="styles['information-div']">
-            <div>姓名:     <input :class="styles.Input" v-model="name"/></div>
-            <div>专业班级: <input :class="styles.Input" v-model="className"/></div>
-            <div>联系方式: <input :class="styles.Input" v-model="phone"/></div>
-            <div>政治面貌: <input :class="styles.Input" v-model="political_status"/></div>
-            <div>邮箱:     <input :class="styles.Input" v-model="email"/></div>
-            <div>家庭住址: <input :class="styles.Input" v-model="address"/></div>
-            <div>职业方向: <input :class="styles.Input" v-model="plan"/></div>
-          </div>
-          <div :class="styles['textarea-div']">
-            <div><span>项目实践经历:        </span><br/><textarea :class="styles.Textarea" v-model="experience"></textarea></div>
-            <div><span>个人荣誉:            </span><br/><textarea :class="styles.Textarea" v-model="honor"></textarea></div>
-            <div><span>个人专业研究兴趣方向: </span><br/><textarea :class="styles.Textarea" v-model="interest"></textarea></div>
-          </div>
+      <card  v-if="form" :class="styles.container">
+        <div style="display: flex;">
+        <div :class="styles.form">
+        <span :class="styles.info">姓名: {{ form.name }}</span>
+        <span :class="styles.info">班级: {{ form.email }}</span>
+        <span :class="styles.info">学号: {{ form.student_id }}</span>
+        <span :class="styles.info">地址: {{ form.address }}</span>
         </div>
+        <div :class="styles.form">
+        <span :class="styles.info">邮箱: {{ form.email }}</span>
+        <span :class="styles.info">电话: {{ form.phone }}</span>
+        <span :class="styles.info">政治面貌: {{ form.political_status}}</span>
+        <span :class="styles.info">职业方向: {{ form.plan }}</span>
+        </div>
+      </div>
+
+      <div :class="styles.form2">
+      <span :class="styles.info">项目实践经历:<br> &ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp; &ensp;&ensp;&ensp;{{ form.experience }}</span>
+        <span :class="styles.info">个人荣誉: <br> &ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;{{ form.honor }}</span>
+        <span :class="styles.info">个人专业研究兴趣方向: <br>&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;{{ form.interest }}</span>
+      </div>
       </card>
     </div>
   </div>
@@ -35,36 +38,44 @@ import { ref } from "vue";
 import { useMainStore } from "@/stores";
 import { ElNotification } from "element-plus";
 import useRequest from "@/apis/useRequest";
+
 isFadingOut.value = false;
+
 const student_id = localStorage.getItem('student_id');
+console.log(student_id)
+
 const userStore = useMainStore().useUserStore();
 const loginStore = useMainStore().useLoginStore();
 
+type StudentForm = {
+  address: string;
+  avatar: string;
+  class: string;
+  email: string;
+  experience: string;
+  honor: string;
+  interest: string;
+  name: string;
+  phone: string;
+  plan: string;
+  political_status: string;
+  student_id: string;
+};
+
+const form = ref<StudentForm>(); // 初始化为undefined
+
 useRequest({
-  data: {student_id:student_id},
+  params: { student_id: student_id },
   method: "GET",
   url: "/api/teacher/studentInfo",
-  manual:false,
+  manual: false,
   headers: { Authorization: loginStore.token },
   onSuccess(response) {
-    console.log(response)
+    form.value = response.data.data; // 请求成功后更新form的值
+    console.log(form.value);
   },
   onError(error) {
     console.log(error);
   },
 });
-
-const name = ref<string|undefined>(userStore.userSession.name);
-const className = ref<string|undefined>(userStore.userSession.class);
-const phone = ref<number|undefined>(userStore.userSession.phone);
-const political_status = ref<string|undefined>(userStore.userSession.political_status);
-const email = ref<string|undefined>(userStore.userSession.email);
-const address = ref<string|undefined>(userStore.userSession.address);
-const plan = ref<string|undefined>(userStore.userSession.plan);
-const honor = ref<string|undefined>(userStore.userSession.honor);
-const experience = ref<string|undefined>(userStore.userSession.experience);
-const interest = ref<string|undefined>(userStore.userSession.interest);
-
- 
-
 </script>
