1099e1c870c0e1eb79b06ef0933da1a0ade71061
xixiibn5100 <1584914306@qq.com>
2024-02-26 19:20:14 +0800

    feat(checkFinishiApproval):新增管理员查看已审批学生的页面

--- src/views/checkFinishedApproval/index.vue
+++ src/views/checkFinishedApproval/index.vue
@@ -11,23 +11,17 @@
           ><Back
         /></el-icon>
       </card>
-      <card :is-fading-out="isFadingOut" :class="styles.studentForm">
-        <el-table :data="filterTableData" style="width: 100% color: #6e6e6e47;;">
-    <el-table-column label="姓名" prop="date" />
-    <el-table-column label="学号" prop="name" />
+      <card :is-fading-out="isFadingOut" :class="styles.studentForm" v-if="studentForm">
+        <el-table :data="filterTableData" style="width: 100%;border-radius: 10px;" >
+    <el-table-column label="姓名" prop="name" />
+    <el-table-column label="学号" prop="student_id" />
     <el-table-column align="right">
       <template #header>
         <el-input v-model="search" size="small" placeholder="查询学号" />
       </template>
       <template #default="scope">
-        <el-button size="small" @click="handleEdit(scope.$index, scope.row)"
-          >Edit</el-button
-        >
-        <el-button
-          size="small"
-          type="danger"
-          @click="handleDelete(scope.$index, scope.row)"
-          >Delete</el-button
+        <el-button size="small" @click="check(scope.row)"
+          >查看双向选择表</el-button
         >
       </template>
     </el-table-column>
@@ -41,12 +35,55 @@
 <script setup lang="ts">
 import styles from "./index.module.scss"
 import { Card, DarkButton } from "@/components/index";
-import { jumpPage, isFadingOut } from "@/tool";
-import { ref } from "vue";
+import { jumpPage, isFadingOut, closeLoading,startLoading } from '@/tool'
+import { ref , computed} from "vue";
 import useRequest from "@/apis/useRequest";
 import { ElNotification } from 'element-plus'
 import { useMainStore } from "@/stores";
-
+const loginStore = useMainStore().useLoginStore()
+const studentForm = ref();
 isFadingOut.value = false
+useRequest({
+    params: {check:2},
+    method: "GET",
+    url: "/api/admin/check",
+    headers: { Authorization: loginStore.token },
+    manual: false,
+    onBefore: () => startLoading(),
+    onSuccess(res) {
+      studentForm.value = res.data.data.data
+      console.log(studentForm.value)
+    },
+    onError(error) {
+      console.log(error);
+    },
+    onFinally: () => closeLoading()
+  });
+const search = ref("")
+const filterTableData = computed(() =>
+  studentForm.value.filter(
+    (data: { student_id: string | string[]; }) =>
+      !search.value ||
+      data.student_id.includes(search.value)
+  )
+)
 
+const check = (rowData: { student_id: any; }) => {
+    useRequest({
+    params: {student_id: rowData.student_id},
+    method: "GET",
+    url: "/api/admin/post",
+    headers: { Authorization: loginStore.token },
+    manual: false,
+    onBefore: () => startLoading(),
+    onSuccess(res) {
+      const url = res.data.data.selection_table
+      window.open(url);
+    },
+    onError(error) {
+      console.log(error);
+    },
+    onFinally: () => closeLoading()
+  });
+}
 </script>
